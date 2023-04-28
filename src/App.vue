ba3249016c79fc480b2f53b11829d1e3993d10f4
unickhow <unick.zhow@gmail.com>
2023-04-29 00:35:03 +0800

    💡 feat: setup default plugin in vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,23 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+
+const count = ref(5)
+
+function handleCreate () {
+  parent.postMessage({ pluginMessage: { type: 'create-rectangles', count: count.value } }, '*')
+}
+
+function handleCancel () {
+  parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*')
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h2>Rectangle Creator</h2>
+  <p>Count: <input type="number" v-model.number="count"></p>
+  <button id="create" @click="handleCreate">Create</button>
+  <button id="cancel" @click="handleCancel">Cancel</button>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    cssCodeSplit: false,
+    rollupOptions: {
+      input: {
+        index: 'index.html',
+        code: 'figma/code.ts'
+      },
+      output: {
+        entryFileNames: '[name].js'
+      }
+    }
+  }
 })
